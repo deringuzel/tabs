7f134845b81bf4d0738b120500f74aa098d616d0
Derin Guzel <dguzel96@gmail.com>
2017-08-23 18:17:35 -0400

    Update Profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,57 +1,82 @@
 import React from 'react';
 import Tbase from './Tbase';
+import ID from './ID';
 
 class Profile extends React.Component{
 	constructor() {
 		super();
+		this.handleChange = this.handleChange.bind(this);
+		this.componentWillMount = this.componentWillMount.bind(this);
+		this.componentWillUnmount = this.componentWillUnmount.bind(this);
 		this.state = {
-			rush: {}
+			rush: null,
 		}
 	}
-	
+
 	componentWillMount() {
-		var ref = Tbase.database().ref('rushes/' + this.props.params.rushName);
-
-		ref.once("value")
-  			.then(function(snapshot) {
-    		var key = snapshot.key; // "ada"
-    		console.log(snapshot.exists());
-    		var name = snapshot.child('name').val();
-    		this.setState({name: name});
-    		console.log(name);
-    		//this.name = snapshot.child('name').val();
-    		console.log(key);
-    		
-  		});
-    		//var childKey = snapshot.child("name/last").key; // "last"
-    	
-
-		//JSON.stringify(ref);//const r = $firebaseObject(ref);
-		//console.log(ref);
-		//this.setState({rush: rush});
+		const reference = 'rushes/' + this.props.params.rushName;
+		this.ref = Tbase.syncState(reference, 
+			{
+				context: this,
+				state: 'rush',
+			});
 	}
 
 	componentWillUnmount() {
 		Tbase.removeBinding(this.ref);
 	}
 
+	handleChange(e) {
+	}
 
 	render() {
+		if (this.state.rush) {
+			return (
+				<div className="catch-of-the-day">
+					<div className="order-wrap">
+						<div>
+							<header className="top">
+								<h1>
+									<span className="ofThe"></span>
+							 	</h1>
+							 	<h3 className="tagline"><span>2017</span></h3>
+							</header>
+						</div>
+						<div>
+							<form ref={(input) => this.fishForm = input} className="fish-edit" onSubmit={(e) => this.handleChange(e)}>
+								<textarea ref={(input) => this.desc = input} placeholder="Comment"></textarea>
+								<button type="submit">Submit Comment</button>
+							</form>
+
+							<form ref={(input) => this.fishForm = input} className="fish-edit" onSubmit={(e) => this.handleChange(e)}>
+								<select ref={(input) => this.status = input}>
+										<option value="available">Fresh!</option>
+										<option value="Unavailable">Sold out!</option>
+								</select>
+								<button type="submit">Submit Rating</button>
+							</form>
+
+							<form ref={(input) => this.fishForm = input} className="fish-edit" onSubmit={(e) => this.handleChange(e)}>
+								<input ref={(input) => this.lastName = input} type="text" placeholder="Against"/>
+								<input ref={(input) => this.name = input} type="text" placeholder="In favor"/>
+								<input ref={(input) => this.name = input} type="text" placeholder="In favor"/>
+								<button type="submit">Submit Clementine Result</button>
+							</form>
+						</div>
+					</div>
+
+					<div className="menu">
+						<ID rush={this.state.rush} tagline="ID"/>
+					</div>		
+				</div>
+			)		
+	} else {
 		return (
-			<div className="order-wrap">
-				<h2>Trash 2017</h2>	
-				<form ref={(input) => this.fishForm = input} className="fish-edit" onSubmit={(e) => this.createRush(e)}>
-					<input ref={(input) => this.name = input} type="text" placeholder=" First Name"/>
-					<input ref={(input) => this.lastName = input} type="text" placeholder="Last Name"/>
-					<input ref={(input) => this.hometown = input} type="text" placeholder="Hometown"/>
-					<input ref={(input) => this.gradDate = input} type="text" placeholder="Graduation Date"/>
-					<input ref={(input) => this.major = input} type="text" placeholder="Major"/>
-					<input ref={(input) => this.room = input} type="text" placeholder="Room"/>
-					<input ref={(input) => this.email = input} type="text" placeholder="Personal Email"/>
-					<button type="submit">Submit</button>
-				</form>
-			</div>	
+			<h1>Trash 2017</h1>	
 			)
+	}
+
+
 	}
 }
 
