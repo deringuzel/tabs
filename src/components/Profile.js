1bcb1c214dacfe7ee64f075ef16a858a2401d784
Derin Guzel <dguzel96@gmail.com>
2017-08-24 14:20:29 -0400

    Update Profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -30,24 +30,23 @@ class Profile extends React.Component{
 	}
 
 	render() {
-		if (this.state.rush) {
+		const rush = this.state.rush;
+		if (rush) {
 			return (
 				<div className="catch-of-the-day">
 					<div className="order-wrap">
 						<div>
 							<header className="top">
-								<h1>
-									<span className="ofThe"></span>
-							 	</h1>
+								<h1><span className="ofThe"></span></h1>
 							 	<h3 className="tagline"><span>2017</span></h3>
 							</header>
 						</div>
+
 						<div>
 							<form ref={(input) => this.fishForm = input} className="fish-edit" onSubmit={(e) => this.handleChange(e)}>
 								<textarea ref={(input) => this.desc = input} placeholder="Comment"></textarea>
 								<button type="submit">Submit Comment</button>
 							</form>
-
 							<form ref={(input) => this.fishForm = input} className="fish-edit" onSubmit={(e) => this.handleChange(e)}>
 								<select ref={(input) => this.status = input}>
 										<option value="available">Fresh!</option>
@@ -55,19 +54,15 @@ class Profile extends React.Component{
 								</select>
 								<button type="submit">Submit Rating</button>
 							</form>
-
-							<form ref={(input) => this.fishForm = input} className="fish-edit" onSubmit={(e) => this.handleChange(e)}>
-								<input ref={(input) => this.lastName = input} type="text" placeholder="Against"/>
+							<form ref={(input) => this.fishForm = input} className="fish-edit" onSubmit={(e) => this.handleChange(e)}>								
+							<input ref={(input) => this.lastName = input} type="text" placeholder="Against"/>
 								<input ref={(input) => this.name = input} type="text" placeholder="In favor"/>
 								<input ref={(input) => this.name = input} type="text" placeholder="In favor"/>
 								<button type="submit">Submit Clementine Result</button>
 							</form>
 						</div>
 					</div>
-
-					<div className="menu">
-						<ID rush={this.state.rush} tagline="ID"/>
-					</div>		
+					<div className="menu"><ID rush={rush} tagline="ID"/></div>		
 				</div>
 			)		
 	} else {
