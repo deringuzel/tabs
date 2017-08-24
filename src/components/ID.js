435deb77a87c82dbb9dc9daf5968ef5f69d21d1b
Derin Guzel <dguzel96@gmail.com>
2017-08-24 14:17:08 -0400

    Update ID.js

--- src/components/ID.js
+++ src/components/ID.js
@@ -1,52 +1,109 @@
 import React from 'react';
+import Information from './Information';
 
 class ID extends React.Component {
-
 	render() {
 		const rush = this.props.rush;
+
 		const comments = this.props.rush.comments;
-		const clementine = this.props.rush.votes;
-		return (
+		const commentsLen = Object.keys(comments).length;
+
+		const votes = this.props.rush.votes;
+		const votesLen = Object.keys(votes).length;
+
+		if(commentsLen === 1 && votesLen > 1) {
+			delete votes["NA"];
+			return (
+					<div>
+						<div><Information rush={rush} tagline="Information"/></div>
+						<div className="boxed">
+							<div className="commentHeader">Votes</div>
+							{
+								Object.keys(votes).map((key, index) => (
+								<div className="warningMessage" key={key}>
+									<ul>{key + ": "} {votes[key]}</ul>
+								</div>
+								 ))
+							}
+						</div>
+						<div className="boxed">
+								<div className="commentHeader">Comments</div>
+								<div className="warningMessage"> No comments yet </div>
+						</div>
+					</div>
+			)
+		} else if (commentsLen > 1 && votesLen === 1) {
+			delete comments["NA"];
+			return (
+					<div>
+						<div><Information rush={rush} tagline="Information"/></div>
+						<div className="boxed">
+								<div className="commentHeader">Votes</div>
+								<div className="warningMessage"> No votes yet </div>
+						</div>
+						<div className="boxed">
+							<div className="commentHeader">Comments</div>
+								{Object.keys(comments).map((key, index) => (
+									<div className="container" key={key}>
+									  <div className="dialogbox">
+									    <div className="body">
+									      <span className="tip tip-left"></span>
+									      <div className="message">	
+											<span>{key + ": "} {comments[key]}</span>
+									      </div>
+									    </div>
+									  </div>
+									</div>))}
+							</div>
+					</div>
+				)
+		} else if (commentsLen === 1 && votesLen === 1){
+			return (
+					<div>
+						<div><Information rush={rush} tagline="Information"/></div>
+						<div className="boxed">
+								<div className="commentHeader">Votes</div>
+								<div className="warningMessage"> No votes yet </div>
+						</div>
+						<div className="boxed">
+								<div className="commentHeader">Comments</div>
+								<div className="warningMessage"> No comments yet </div>
+						</div>
+					</div>
+				)
+		} else {
+			delete votes["NA"];
+			delete comments["NA"];
+			return (
 					<div className="order-wrap">
-						<h1>{rush.name} {rush.lastName}</h1>
-						<h6>{"Rating: " + rush.rating}</h6>
-						<div className="fish-edit">
-							<input type="text" name="name" value={rush.name} placeholder="First Name" 
-							onChange={(e) => this.handleChange(e)}/>
-							<input type="text" name="lastname" value={rush.lastName} placeholder="Last Name"
-							onChange={(e) => this.handleChange(e)}/>
-							<input type="text" name="hometown" value={rush.hometown} placeholder="Hometown"
-							onChange={(e) => this.handleChange(e)}/>
-							<input type="text" name="graddate" value={rush.gradDate} placeholder="Graduation Date"
-							onChange={(e) => this.handleChange(e)}/>
-							<input type="text" name="major" value={rush.major} placeholder="Major"
-							onChange={(e) => this.handleChange(e)}/>
-							<input type="text" name="room" value={rush.room} placeholder="Room"
-							onChange={(e) => this.handleChange(e)}/>
-							<input type="text" name="email" value={rush.email} placeholder="Personal Email"
-							onChange={(e) => this.handleChange(e)}/>
-						</div>
-						<div className="fish-edit">
-							<input type="text" name="events" value={"Events attended: " + rush.event} placeholder="Events Attended"
-							onChange={(e) => this.handleChange(e)}/>
-						</div>
-						<div className="fish-edit">
+						<div><Information rush={rush} tagline="Information"/></div>	
+						<div className="boxed">
+							<div className="commentHeader">Votes</div>
 							{
-								Object.keys(clementine).map((key, index) => (
-								<button key={index}>
-								 {key + ": "} {clementine[key]}</button>))
+								Object.keys(votes).map((key, index) => (
+								<div className="warningMessage" key={key}>
+									<ul>{key + ": "} {votes[key]}</ul>
+								</div>
+								 ))
 							}
 						</div>
-						<div className="fish-edit">
-							{
-								Object.keys(comments).map((key, index) => (
-								<button key={index}>
-								 {key + ": "} {comments[key]}</button>))
-							}
+						<div className="boxed">
+							<div className="commentHeader">Comments</div>
+							{Object.keys(comments).map((key, index) => (
+								<div className="container" key={key}>
+									 <div className="dialogbox">
+									    <div className="body">
+									      <span className="tip tip-left"></span>
+									      <div className="message">	
+											<span>{key + ": "} {comments[key]}</span>
+									      </div>	
+								    	</div>
+									</div>
+								</div>))}
 						</div>
 					</div>			
-		)
+			)
+		}
 	}
 }
-
 export default ID;
