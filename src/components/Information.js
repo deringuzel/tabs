1fced5db064d9bbbf05dd1b91cb90e286bf00db7
Derin Guzel <dguzel96@gmail.com>
2017-08-25 16:25:25 -0400

    Update Information.js

--- src/components/Information.js
+++ src/components/Information.js
@@ -3,11 +3,16 @@ import React from 'react';
 class Information extends React.Component {
 	render() {
 		const rush = this.props.rush;
+		var eventString = '';
+		for (var i = 0; i < rush.event.length - 1; i++) {
+			eventString += rush.event[i] + ", ";
+		}
+		eventString += rush.event[rush.event.length - 1];
+
 		return (
 
 				<div>
 						<h1>{rush.name} {rush.lastName}</h1>
-						<h6>{"Rating: " + rush.rating}</h6>
 						<div className="commentHeader">Info</div>
 						<table>
 							<tbody>
@@ -23,10 +28,9 @@ class Information extends React.Component {
 			  					</tr>
 			  				</tbody>
 						</table>
-						
 						<div className="boxed">
 								<div className="commentHeader">Events</div>
-								<div className="warningMessage">{rush.event}</div>
+								<div className="warningMessage">{eventString}</div>
 						</div>
 				</div>
 				
