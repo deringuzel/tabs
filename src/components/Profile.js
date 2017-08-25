69f0aebca487c2ec03521a22e660c5eedb896493
Derin Guzel <dguzel96@gmail.com>
2017-08-25 16:22:15 -0400

    Add functionality to submit comment, vote, opinion
    handleOpinion goes into an infinte loop must be fixed

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -5,7 +5,15 @@ import ID from './ID';
 class Profile extends React.Component{
 	constructor() {
 		super();
-		this.handleChange = this.handleChange.bind(this);
+		this.handleComment = this.handleComment.bind(this);
+		this.updateComments = this.updateComments.bind(this);
+
+		this.handleVote = this.handleVote.bind(this);
+		this.updateVotes = this.updateVotes.bind(this);
+
+		this.handleOpinion = this.handleOpinion.bind(this);
+		this.updateOpinion = this.updateOpinion.bind(this);
+
 		this.componentWillMount = this.componentWillMount.bind(this);
 		this.componentWillUnmount = this.componentWillUnmount.bind(this);
 		this.state = {
@@ -26,11 +34,79 @@ class Profile extends React.Component{
 		Tbase.removeBinding(this.ref);
 	}
 
-	handleChange(e) {
+	handleComment() {
+		const today = new Date();
+    	const key = this.props.params.membername + ", " + today;
+    	const comments = this.state.rush.comments;
+		const updatedComments = {...comments,
+			[key] : this.comment.value
+		}
+		this.updateComments(updatedComments);
+	}
+
+	updateComments(updatedComments) {
+		const rush = {...this.state.rush};
+		rush.comments = updatedComments;
+		this.setState({rush});
+	}
+
+	handleVote() {
+		//TODO: change to rush chairs
+		if (this.props.params.membername === 'deringuzel') {
+			const today = new Date();
+	    	const key = (today.getMonth() + 1) + '-' + today.getDate() + '-' + today.getFullYear();
+
+	    	var inFavor = parseInt(this.inFavor.value, 10);
+	    	var against = parseInt(this.against.value, 10);
+
+	    	var value = 'For: ' + inFavor + ' Against: ' + against;
+
+	    	if(against > inFavor) {
+	    		value += " CUT";
+	    	}
+
+	    	const votes = this.state.rush.votes;
+	    	const updatedVotes = {...votes,
+	    		[key] : value
+	    	}
+	    	this.updateVotes(updatedVotes, "CUT", inFavor, against);
+    	}
+	}
+
+	updateVotes(updatedVotes, s, inFavor, against) {
+		const rush = {...this.state.rush};
+		rush.votes = updatedVotes;
+		if(against > inFavor) {
+			rush.cut = s;
+		}
+		this.setState({rush});
+	}
+
+	handleOpinion() {
+		const rating = this.state.rush.rating;
+		const value = rating[this.opinion.value];
+		console.log(value); 
+		console.log("hello");
+		const updatedRating = {...rating,
+			[this.opinion.value] : value
+		}
+		this.handleOpinion(updatedRating);
+	}
+
+	updateOpinion(updatedRating) {
+		const rush = {...this.state.rush};
+		rush.rating = updatedRating;
+		this.setState({rush});
 	}
 
 	render() {
 		const rush = this.state.rush;
+		//TODO: change to rush comittee
+		if (this.props.params.membername === 'deringuzel') {
+			this.auth = "Submit Vote";
+		} else {
+			this.auth = "WARNING: ONLY RUSH CHAIRS CAN SUBMIT!"
+		}
 		if (rush) {
 			return (
 				<div className="catch-of-the-day">
@@ -41,24 +117,23 @@ class Profile extends React.Component{
 							 	<h3 className="tagline"><span>2017</span></h3>
 							</header>
 						</div>
-
 						<div>
-							<form ref={(input) => this.fishForm = input} className="fish-edit" onSubmit={(e) => this.handleChange(e)}>
-								<textarea ref={(input) => this.desc = input} placeholder="Comment"></textarea>
-								<button type="submit">Submit Comment</button>
-							</form>
-							<form ref={(input) => this.fishForm = input} className="fish-edit" onSubmit={(e) => this.handleChange(e)}>
-								<select ref={(input) => this.status = input}>
-										<option value="available">Fresh!</option>
-										<option value="Unavailable">Sold out!</option>
+							<form className="fish-edit" onSubmit={() => this.handleOpinion()}>
+								<select ref={(input) => this.opinion = input}>
+										<option value="love">Love!</option>
+										<option value="hate">Hate</option>
+										<option value="NA">Don't know</option>
 								</select>
 								<button type="submit">Submit Rating</button>
 							</form>
-							<form ref={(input) => this.fishForm = input} className="fish-edit" onSubmit={(e) => this.handleChange(e)}>								
-							<input ref={(input) => this.lastName = input} type="text" placeholder="Against"/>
-								<input ref={(input) => this.name = input} type="text" placeholder="In favor"/>
-								<input ref={(input) => this.name = input} type="text" placeholder="In favor"/>
-								<button type="submit">Submit Clementine Result</button>
+							<form className="fish-edit" onSubmit={() => this.handleComment()}>
+								<textarea ref={(input) => this.comment = input} placeholder="Comment"></textarea>
+								<button type="submit">Submit Comment</button>
+							</form>
+							<form className="fish-edit" onSubmit={() => this.handleVote()}>
+								<input ref={(input) => this.inFavor = input} type="number" placeholder="In favor"/>								
+								<input ref={(input) => this.against = input} type="number" placeholder="Against"/>
+								<button type="submit">{this.auth}</button>
 							</form>
 						</div>
 					</div>
@@ -70,7 +145,6 @@ class Profile extends React.Component{
 			<h1>Trash 2017</h1>	
 			)
 	}
-
 
 	}
 }
