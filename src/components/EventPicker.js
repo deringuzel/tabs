24a00fbfc36e7fa4d4764365564b2c5bff9fc016
Derin Guzel <dguzel96@gmail.com>
2017-08-29 15:52:31 -0400

    Update EventPicker.js

--- src/components/EventPicker.js
+++ src/components/EventPicker.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import Tbase from './Tbase';
 
+//TODO: ask yoni about mounting and not mounting problem
+
 class EventPicker extends React.Component {
 	constructor() {
 		super();
 		this.renderLogin = this.renderLogin.bind(this);
 		this.authenticate = this.authenticate.bind(this);
 		this.authHandler = this.authHandler.bind(this);
+		this.renderWelcome = this.renderWelcome.bind(this);
+		this.logout = this.logout.bind(this);
 		this.state = {
 			uid: null,
+			username: null, 
 		}
 	}
 
@@ -27,8 +32,7 @@ class EventPicker extends React.Component {
 	}
 
 	authenticate() {
-		Tbase.authWithOAuthPopup("facebook", this.authHandler);
-		//this.context.router.transitionTo(`/tmember/derin`);
+		Tbase.authWithOAuthPopup("facebook", this.authHandler); 
 	}
 
 	authHandler(err, authData) {
@@ -36,39 +40,70 @@ class EventPicker extends React.Component {
 			console.log(err);
 			return;
 		} 
+		var username = authData.user.displayName.toLowerCase();
+		username = username.replace(/\s/g, '');
 		this.setState({
-			uid: authData.user.uid
+			uid: authData.user.uid,
+			username: username
 		});
+		this.renderWelcome();
 	}
 		
 	renderLogin() {
-		//this.authenticate
 		return (
 			<nav className="login">
-				<button className="facebook" onClick={() => 
-					this.context.router.transitionTo(`/tmember/derin/search`)}>
+				<button className="facebook" onClick={() => this.authenticate()}>
 				T Login</button>
 			</nav>
 		)
 	}
 
-	render() {
-		//if(!this.state.uid) {
-		//	return <div>{this.renderLogin()}</div>
-		//}
-		return (
-			<div>
-				<form className="event-selector" onSubmit={this.goToEvent.bind(this)}>
-				<h2>Event</h2>
-				<input type="text" required placeholder="Event Name"
-					 ref={(input) => {this.eventInput= input}}/>
-				<button type="submit">Create Event</button>
-				</form>
-				<div>{this.renderLogin()}</div>
-			</div>
+	logout() {
+		Tbase.unauth();
+		this.setState({
+			uid: null,
+			username: null
+		});
+	}
 
+	renderWelcome() {
+		return(
+			<nav className="login">
+				<div>
+					<button className="facebook" onClick={() => 
+						this.context.router.transitionTo(`/tmember/${this.state.username}/search`)}>
+					Welcome  {this.state.username}</button>
+					<button onClick={() => this.logout()}>Log out</button>
+				</div>
+			</nav>
 			)
-		
+	}
+
+	render() {
+		if(this.state.username) {
+			return (
+				<div>
+					<form className="event-selector" onSubmit={this.goToEvent.bind(this)}>
+					<h2>Event</h2>
+					<input type="text" required placeholder="Event Name"
+						 ref={(input) => {this.eventInput= input}}/>
+					<button type="submit">Create Event</button>
+					</form>
+					<div>{this.renderWelcome()}</div>
+				</div>
+			)
+		} else {
+			return (
+				<div>
+					<form className="event-selector" onSubmit={this.goToEvent.bind(this)}>
+					<h2>Event</h2>
+					<input type="text" required placeholder="Event Name" ref={(input) => {this.eventInput= input}}/>
+					<button type="submit">Create Event</button>
+					</form>
+					<div>{this.renderLogin()}</div>
+				</div>
+				)
+		}
 	}
 }
 
