7cd4f5ba3ed08b424703066192ecdf9af96b8b01
Derin Guzel <dguzel96@gmail.com>
2017-08-29 15:55:18 -0400

    Update Register.js

--- src/components/Register.js
+++ src/components/Register.js
@@ -13,24 +13,31 @@ class Register extends React.Component{
 			major: this.major.value,
 			room: this.room.value,
 			email: this.email.value,
-			cut: false,
-			event: []
-			
+			cut: 'Not cut',
+			event: [],
+			comments: {"NA": "No comments yet"},
+			rating: {
+				love: 0,
+				hate: 0,
+				NA: 0
+			}, 
+			votes: {"NA": "No clementine yet"},
+			pictures: []
 		}
 
 		rush.event.push(this.props.event);
 		
 		const name = rush.name + rush.lastName;
 
-		this.props.addRush(rush, name);
-		this.fishForm.reset();
+		this.props.addRush(rush, name.toLowerCase());
+		this.rushForm.reset();
 	}
 
 	render() {
 		return (
 			<div className="order-wrap">
 				<h2>Register Here</h2>	
-				<form ref={(input) => this.fishForm = input} className="fish-edit" onSubmit={(e) => this.createRush(e)}>
+				<form ref={(input) => this.rushForm = input} className="fish-edit" onSubmit={(e) => this.createRush(e)}>
 					<input ref={(input) => this.name = input} type="text" placeholder=" First Name"/>
 					<input ref={(input) => this.lastName = input} type="text" placeholder="Last Name"/>
 					<input ref={(input) => this.hometown = input} type="text" placeholder="Hometown"/>
@@ -40,10 +47,14 @@ class Register extends React.Component{
 					<input ref={(input) => this.email = input} type="text" placeholder="Personal Email"/>
 					<button type="submit">Submit</button>
 				</form>
+				<button className="facebook" onClick={() => this.context.router.transitionTo(`/`)}>Home</button>
 			</div>	
 		)
 	}
 }
 
-export default Register;
+Register.contextTypes = {
+	router: React.PropTypes.object
+}
 
+export default Register;
